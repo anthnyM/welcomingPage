41c4515267ea6d67fd4d736873ad6c0b930e03e3
antonio <antonio.ontiveros@ucb.edu.bo>
2024-08-16 17:18:32 -0400

    add gender requirement

--- src/presenter.js
+++ src/presenter.js
@@ -1,6 +1,7 @@
 import saludar from "./saludador";
 
 const nombre = document.querySelector("#nombre");
+const genero = document.querySelector("#genero")
 const form = document.querySelector("#user-form");
 const div = document.querySelector("#divNombre");
 
@@ -8,6 +9,7 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const nombreUsuario = nombre.value
+  const generoUsuario = genero.value
 
-  div.innerHTML = "<p>" + saludar(nombreUsuario) + "</p>";
+  div.innerHTML = "<p>" + saludar(nombreUsuario, generoUsuario) + "</p>";
 });
--- src/saludador.js
+++ src/saludador.js
@@ -1,5 +1,12 @@
-function saludar(nombre) {
-    return "Hola " + nombre
+function saludar(nombre, genero) {
+    let msg
+    if (genero == "masculino") {
+        msg = "Bienvenido "
+    }
+    else if (genero == "femenino") {
+        msg = "Bienvenida "
+    }
+    return msg + nombre
 }
 
 export default saludar
