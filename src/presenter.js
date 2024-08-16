0c956123b74e3e821edfbf576def52ea18953874
antonio <antonio.ontiveros@ucb.edu.bo>
2024-08-16 19:36:42 -0400

    add age requirement

--- src/presenter.js
+++ src/presenter.js
@@ -1,7 +1,8 @@
 import saludar from "./saludador";
 
 const nombre = document.querySelector("#nombre");
-const genero = document.querySelector("#genero")
+const genero = document.querySelector("#genero");
+const edad = document.querySelector("#edad")
 const form = document.querySelector("#user-form");
 const div = document.querySelector("#divNombre");
 
@@ -10,6 +11,7 @@ form.addEventListener("submit", (event) => {
 
   const nombreUsuario = nombre.value
   const generoUsuario = genero.value
+  const edadUsuario = Number.parseInt(edad.value)
 
-  div.innerHTML = "<p>" + saludar(nombreUsuario, generoUsuario) + "</p>";
+  div.innerHTML = "<p>" + saludar(nombreUsuario, generoUsuario, edadUsuario) + "</p>";
 });
--- src/saludador.js
+++ src/saludador.js
@@ -1,12 +1,22 @@
-function saludar(nombre, genero) {
+function saludar(nombre, genero, edad) {
     let msg
+    let sus
     if (genero == "masculino") {
         msg = "Bienvenido "
     }
     else if (genero == "femenino") {
         msg = "Bienvenida "
     }
-    return msg + nombre
+    if (edad < 35) {
+        sus = "joven "
+    }
+    else if(genero == "femenino" && edad >= 35){
+        sus = "señorita "
+    }
+    else {
+        sus = "señor "
+    }
+    return msg + sus + nombre
 }
 
 export default saludar
